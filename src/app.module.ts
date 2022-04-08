5f92427a9c80f7122aceb768b97cb6b64de82da4
narus <narus.od@gmail.com>
2022-04-08 16:16:23 +0300

    add mail servise

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,28 @@ import { AppService } from './app.service';
 import { PeopleModule } from './people/people.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService  } from '@nestjs/config';
+
 import { MongooseConfigService } from './config/mongoose-config-service';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { MailCreatedListener } from './people/listeners/mail.listerner';
+import { MailModule } from './mail/mail.module';
+
+import { MailerModule } from '@nestjs-modules/mailer';
+import { getMailConfig } from './config/mail-config';
+
 
 @Module({
   imports: [PeopleModule,
     ConfigModule.forRoot(),
     MongooseModule.forRootAsync({useClass: MongooseConfigService}),
-    EventEmitterModule.forRoot()
+    EventEmitterModule.forRoot(),
+    MailModule,
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getMailConfig,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService, MailCreatedListener]
